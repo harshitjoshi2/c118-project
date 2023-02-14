6922e73beb817065b281b5caa6fec787352022e9
harshitjoshi2 <89769444+harshitjoshi2@users.noreply.github.com>
2023-02-14 21:36:18 +0530

    Add files via upload

---
+++ PRO-C118-Project-main/app.py
@@ -0,0 +1,54 @@
+from flask import Flask , render_template , request , jsonify
+import prediction
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# api listening to POST requests and predicting sentiments
+@app.route('/predict' , methods = ['POST'])
+def predict():
+
+    response = ""
+    review = request.json.get('customer_review')
+    if not review:
+        response = {'status' : 'error',
+                    'message' : 'Empty Review'}
+    
+    else:
+
+        # calling the predict method from prediction.py module
+        sentiment , path = prediction.predict(review)
+        response = {'status' : 'success',
+                    'message' : 'Got it',
+                    'sentiment' : sentiment,
+                    'path' : path}
+
+    return jsonify(response)
+
+
+# Creating an API to save the review, user clicks on the Save button
+@app.route('/save' , methods = ['POST'])
+def save():
+
+ 
+    date = request.json.get('date')
+    product = request.json.get('product')
+    review = request.json.get('review')
+    sentiment = request.json.get('sentiment')
+
+ 
+    data_entry = date + "," + product + "," + review + "," + sentiment
+
+    f = open('./static/assets/datafiles/data_entry.csv' , 'a')
+
+    f.write(data_entry + '\n')
+
+    return jsonify({'status' : 'success' , 
+                    'message' : 'Data Logged'})
+
+
+if __name__  ==  "__main__":
+    app.run(debug = True)
